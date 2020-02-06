9688b490aff1bc52373aee9362bc61f0e9269d5d
Edhotmetal <edward@flashnetsolutions.com>
2020-02-06 18:16:12 +0000

    Change order of post fields on admin page
    This commit is just to try out something I learned.
    We can change the order of the fields of a model in the admin page. By
    default, the admin page displays the fields in the same order as the
    model but if we create a new class in admin.py we can change that order.

--- learning_users/blog/admin.py
+++ learning_users/blog/admin.py
@@ -3,5 +3,10 @@ from blog.models import Post,Comment
 
 # Register your models here.
 
-admin.site.register(Post)
+# Change the way posts are displayed on the admin page
+class PostAdmin(admin.ModelAdmin):
+
+    fields = ['author', 'title', 'create_date', 'published_date','text']
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
