9c2e839ebc955c9e7f542fc63f41a14d6d5ac840
Edhotmetal <edward@flashnetsolutions.com>
2020-03-03 15:37:25 +0000

    Insert images into blog posts! I added the ability to insert images into blog posts. Right now, you can only type a url but I plan to allow uploads next. It was a real pain modifying the tinyMCE editor to add this funcitonality. I had my custom.js file in the static directory like it should be so I simply added the image plugin. However, no changes made any difference so I deleted it. Somehow Django could still find the file after I deleted it? I ended up creating custom.js in a different directory and editing it there.

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.abspath(os.path.join(BASE_DIR, 'media'))
 
 # Quick-start development settings - unsuitable for production
@@ -131,6 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR,os.path.join(BASE_DIR,'/blog/static')]
 STATIC_URL = '/static/'
 
